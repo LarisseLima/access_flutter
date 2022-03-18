e83abe20392f429020fe659b87608ee23fec4e92
Larisse Lima <larisse.lima2@gmail.com>
2022-03-18 10:39:48 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'home/home_page.dart';
 import 'home/http/http_bindings.dart';
 import 'home/http/http_page.dart';
@@ -10,7 +9,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
